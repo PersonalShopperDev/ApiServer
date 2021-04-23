228a6df84c19a9a4dd23eb6b339fa06b8a3eab1e
HoYean Lee <ghdusjang8@gmail.com>
2021-04-23 16:34:27 +0900

    feat: 기본 패키지 추가

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, {Request, Response, NextFunction} from 'express';
+import bodyParser from "body-parser";
+import env from "dotenv";
+import logger from 'morgan';
+import createError from 'http-errors';
+
+const app = express();
+
+env.config();
+
+app.set("port", process.env.PORT || 3000);
+if (process.env.NODE_MODE === "development") app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.send('hello!');
+});
+
+app.use(function (req: express.Request, res: express.Response) {
+    res.send(404);
+});
+
+export default app;
