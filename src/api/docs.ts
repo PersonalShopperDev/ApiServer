cd6d879d40245a0d228be9f7609937b807b233fe
HoYean Lee <ghdusjang8@gmail.com>
2021-04-26 17:31:39 +0900

    fix: EB 자동 배포 처리
    "npm run deploy" 를 통해서 ts 컴파일 후 배포 처리를 한다.
    morgan이랑 redoc-express를 production level로 올림.
    맘 같아서는 제거 하고 싶은데 방법을 찾지 못함.

---
+++ src/api/docs.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from 'express'
+import redoc from 'redoc-express'
+
+const router = express.Router()
+
+router.get(
+  '/',
+  redoc({
+    title: 'API Docs',
+    specUrl: '/docs/swagger',
+  }),
+)
+
+router.get('/swagger', (req: Request, res: Response) => {
+  res.sendFile('swagger.yaml', { root: '.' })
+})
+
+export default process.env.NODE_MODE === 'development' ? router : null
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,9 @@
 import express, { Request, Response, NextFunction } from 'express'
 import bodyParser from 'body-parser'
-import env from 'dotenv'
 import logger from 'morgan'
-import redoc from 'redoc-express'
+import docRouter from './api/docs'
 
 const app = express()
-
-env.config()
 
 app.set('port', process.env.PORT || 3000)
 if (process.env.NODE_MODE === 'development') app.use(logger('dev'))
@@ -14,27 +11,15 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/health', (req: express.Request, res: express.Response) => {
-  res.status(200)
+  res.sendStatus(200)
 })
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send('hello!')
 })
-
-// serve your swagger.json file
-app.get('/docs/swagger', (req, res) => {
-  res.sendFile('swagger.yaml', { root: '.' })
-})
-
-app.get(
-  '/docs',
-  redoc({
-    title: 'API Docs',
-    specUrl: '/docs/swagger',
-  }),
-)
+if (docRouter) app.use('/docs', docRouter)
 
 app.use(function (req: express.Request, res: express.Response) {
-  res.send(404)
+  res.sendStatus(404)
 })
 
 export default app
