cf349909b981a7ad4fc552a63a57a1f641de9152
HoYean Lee <ghdusjang8@gmail.com>
2021-04-26 14:46:55 +0900

    feat: Auth Api Docs 추가

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Request, Response, NextFunction } from 'express'
 import bodyParser from 'body-parser'
 import env from 'dotenv'
 import logger from 'morgan'
-import createError from 'http-errors'
+import redoc from 'redoc-express'
 
 const app = express()
 
@@ -19,6 +19,19 @@ app.use('/health', (req: express.Request, res: express.Response) => {
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send('hello!')
 })
+
+// serve your swagger.json file
+app.get('/docs/swagger', (req, res) => {
+  res.sendFile('swagger.yaml', { root: '.' })
+})
+
+app.get(
+  '/docs',
+  redoc({
+    title: 'API Docs',
+    specUrl: '/docs/swagger',
+  }),
+)
 
 app.use(function (req: express.Request, res: express.Response) {
   res.send(404)
