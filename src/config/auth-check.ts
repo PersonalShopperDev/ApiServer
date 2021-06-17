53dd606c56e244b910dd858d8acfafe56edfad8b
HoYean Lee <comforest@naver.com>
2021-06-17 22:16:41 +0900

    fix: Auth check Error 수정
    - expire date 비교를 반대로 함

--- src/config/auth-check.ts
+++ src/config/auth-check.ts
@@ -2,31 +2,39 @@ import jwt from 'jsonwebtoken'
 import { NextFunction, Request, Response } from 'express'
 
 export default (req: Request, res: Response, next: NextFunction) => {
-  if (req.header('Authorization') === 'test') {
+  const auth = req.header('Authorization')
+  if (auth === 'test') {
     req['auth'] = { userId: 54 }
     next()
     return
   }
   try {
-    const auth = req.header('Authorization')
-    const list = auth!.split(' ')
+    if (auth == null) {
+      error401(res, next)
+      return
+    }
+    const list = auth.split(' ')
     if (list[0].toLowerCase() != 'bearer') {
-      throw Error()
+      error401(res, next)
+      return
     }
 
     const token = list[1]
     const verify = jwt.verify(token, process.env.JWT_KEY)
 
-    if (verify.exp > new Date().getTime() * 0.001) {
-      res.sendStatus(401)
-      next(401)
+    if (verify.exp < new Date().getTime() * 0.001) {
+      error401(res, next)
       return
     }
 
     req['auth'] = verify
     next()
   } catch (e) {
-    res.sendStatus(401)
-    next(401)
+    error401(res, next)
   }
+}
+
+const error401 = (res: Response, next: NextFunction) => {
+  res.sendStatus(401)
+  next(401)
 }
